5f44baf26c1ea3559ccdb9b3ed4d8f6398a1a1a1
e <bright.li@9client.com>
2019-06-14 15:24:05 +0800

    First commit.

---
+++ spring-boot-test/src/main/java/lee/bright/spring/boot/test/TestApplication.java
@@ -0,0 +1,17 @@
+package lee.bright.spring.boot.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 应用的主入口。
+ * @author Bright Lee
+ */
+@SpringBootApplication
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApplication.class, args);
+	}
+
+}
