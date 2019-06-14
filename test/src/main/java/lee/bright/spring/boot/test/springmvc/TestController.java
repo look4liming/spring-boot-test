76f54201c9ccb7f4a099be144894fdadde071d91
e <bright.li@9client.com>
2019-06-14 16:36:42 +0800

    2019-06-14 16:36

--- test/src/main/java/lee/bright/spring/boot/test/springmvc/TestController.java
+++ test/src/main/java/lee/bright/spring/boot/test/springmvc/TestController.java
@@ -1,18 +1,17 @@
 package lee.bright.spring.boot.test.springmvc;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Spring boot 集成 Spring MVC。
  * @author Bright Lee
  */
-@Controller
+@RestController
 @RequestMapping("/spring-mvc")
 public class TestController {
 	
@@ -21,7 +20,6 @@ public class TestController {
 	 * 访问该链接地址，会在浏览器上显示“Hello!”。
 	 */
 	@RequestMapping("/hello")
-	@ResponseBody
 	public String hello() {
 		return "Hello!";
 	}
@@ -31,7 +29,6 @@ public class TestController {
 	 * 访问该链接地址，会在浏览器上显示“Hello Mike!”。
 	 */
 	@RequestMapping("/helloUser/{userName}")
-	@ResponseBody
 	public String helloUser(@PathVariable String userName) {
 		return "Hello " + userName + "!";
 	}
@@ -41,7 +38,6 @@ public class TestController {
 	 * 访问该链接地址，会在浏览器上显示“Your gender is male.”。
 	 */
 	@RequestMapping("/helloGender")
-	@ResponseBody
 	public String helloGender(@RequestParam String gender) {
 		return "Your gender is " + gender + ".";
 	}
@@ -51,7 +47,6 @@ public class TestController {
 	 * 访问该链接地址，会在浏览器上显示HTTP请求头信息。
 	 */
 	@RequestMapping("/printRequestHeaderHost")
-	@ResponseBody
 	public String printRequestHeaderHost(@RequestHeader("Host") String host) {
 		return "Host: " + host;
 	}
@@ -61,9 +56,20 @@ public class TestController {
 	 * 访问该链接地址，会在浏览器上显示JSESSIONID的值。
 	 */
 	@RequestMapping("/printCookieJsessionId")
-	@ResponseBody
 	public String printCookieJsessionId(@CookieValue("JSESSIONID") String jSessionId) {
 		return "JSESSIONID: " + jSessionId;
+	}
+	
+	/**
+	 * http://127.0.0.1:1234/spring-boot-test/spring-mvc/printJson<br/>
+	 * 访问该链接地址，会在浏览器上显示TestBean对象的JSON字符串。
+	 */
+	@RequestMapping("printJson")
+	public TestBean printJson() {
+		TestBean bean = new TestBean();
+		bean.setField1("F-1");
+		bean.setField2("F-2");
+		return bean;
 	}
 
 }
