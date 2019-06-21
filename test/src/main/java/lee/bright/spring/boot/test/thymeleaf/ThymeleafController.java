081e2af55528bf022b6ed0f0e3f260b1dae8bcc8
e <bright.li@9client.com>
2019-06-21 16:21:49 +0800

    Thymeleaf

---
+++ test/src/main/java/lee/bright/spring/boot/test/thymeleaf/ThymeleafController.java
@@ -0,0 +1,54 @@
+package lee.bright.spring.boot.test.thymeleaf;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/thymeleaf")
+public class ThymeleafController {
+	
+	/**
+	 * “http://127.0.0.1:1234/spring-boot-test/thymeleaf/hello”<br/>
+	 * 返回的字符串代表一个Thymeleaf模板文件的名字。<br/>
+	 * 如本例，将自动跳转到classpath:/templates/hello-thymeleaf.html。
+	 */
+	@RequestMapping("/hello")
+	public String hello(Map<String, Object> model) {
+		model.put("name", "Thymeleaf模板引擎");
+		
+		Person person = new Person();
+		person.setName("张三");
+		person.setGender("男");
+		model.put("person", person);
+		
+		model.put("h1", "<h1>aaaaaaaaaaaaa</h1>");
+		
+		return "hello-thymeleaf";
+	}
+	
+	public static class Person {
+		
+		private String name;
+		private String gender;
+		
+		public String getName() {
+			return name;
+		}
+		
+		public void setName(String name) {
+			this.name = name;
+		}
+		
+		public String getGender() {
+			return gender;
+		}
+		
+		public void setGender(String gender) {
+			this.gender = gender;
+		}
+		
+	}
+
+}
