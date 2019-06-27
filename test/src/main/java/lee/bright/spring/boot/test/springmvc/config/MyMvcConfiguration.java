0ec8087f4aa7a7ddf425a6b84c46106d401cfd5e
e <bright.li@9client.com>
2019-06-27 18:32:55 +0800

    2019-06-27 18:32

---
+++ test/src/main/java/lee/bright/spring/boot/test/springmvc/config/MyMvcConfiguration.java
@@ -0,0 +1,49 @@
+package lee.bright.spring.boot.test.springmvc.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.context.request.WebRequestInterceptor;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class MyMvcConfiguration extends WebMvcConfigurationSupport {
+	
+	private static final Logger logger = LoggerFactory.getLogger(MyMvcConfiguration.class);
+	
+	public MyMvcConfiguration() {
+	}
+	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		// TODO
+	}
+	
+	@Override
+	public void addFormatters(FormatterRegistry registry) {
+		// TODO
+	}
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		WebRequestInterceptor interceptor = new MyWebRequestInterceptor();
+		registry.addWebRequestInterceptor(interceptor);
+		logger.info("addInterceptors===>{}", interceptor);
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		// TODO
+	}
+	
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		// TODO
+	}
+
+}
---
+++ test/src/main/java/lee/bright/spring/boot/test/springmvc/config/MyWebRequestInterceptor.java
@@ -0,0 +1,31 @@
+package lee.bright.spring.boot.test.springmvc.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.context.request.WebRequestInterceptor;
+
+public class MyWebRequestInterceptor implements WebRequestInterceptor {
+	
+	private static final Logger logger = LoggerFactory.getLogger(MyWebRequestInterceptor.class);
+
+	@Override
+	public void afterCompletion(WebRequest request, Exception exception)
+			throws Exception {
+		logger.info("afterCompletion...");
+		String abc = request.getParameter("abc");
+		logger.info("abc===>{}", abc);
+	}
+
+	@Override
+	public void postHandle(WebRequest request, ModelMap modelMap) throws Exception {
+		logger.info("postHandle...");
+	}
+
+	@Override
+	public void preHandle(WebRequest request) throws Exception {
+		logger.info("preHandle...");
+	}
+
+}
