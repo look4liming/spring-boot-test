bddb82ef77918f44cb51f032f4b2ed538d8d15d8
e <bright.li@9client.com>
2019-06-27 18:35:19 +0800

    2019-06-27 18:35

--- test/src/main/java/lee/bright/spring/boot/test/springmvc/config/MyWebMvcConfiguration.java
+++ test/src/main/java/lee/bright/spring/boot/test/springmvc/config/MyWebMvcConfiguration.java
@@ -12,11 +12,11 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @Configuration
-public class MyMvcConfiguration extends WebMvcConfigurationSupport {
+public class MyWebMvcConfiguration extends WebMvcConfigurationSupport {
 	
-	private static final Logger logger = LoggerFactory.getLogger(MyMvcConfiguration.class);
+	private static final Logger logger = LoggerFactory.getLogger(MyWebMvcConfiguration.class);
 	
-	public MyMvcConfiguration() {
+	public MyWebMvcConfiguration() {
 	}
 	
 	@Override
