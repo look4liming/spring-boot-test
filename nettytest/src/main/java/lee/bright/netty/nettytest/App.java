8d5d0e848acdd0c11f169eabcab2c025b622170c
e <bright.li@9client.com>
2019-07-11 14:58:56 +0800

    2019-07-11 14:58

---
+++ nettytest/src/main/java/lee/bright/netty/nettytest/App.java
@@ -0,0 +1,13 @@
+package lee.bright.netty.nettytest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
--- test/src/main/java/lee/bright/spring/boot/test/thymeleaf/LoginController.java
+++ test/src/main/java/lee/bright/spring/boot/test/thymeleaf/LoginController.java
@@ -1,9 +1,6 @@
 package lee.bright.spring.boot.test.thymeleaf;
 
-import java.util.Map;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
